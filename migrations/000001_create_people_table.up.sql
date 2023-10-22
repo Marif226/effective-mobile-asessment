905c1f07e07dec2654aa81011306a96d51ea840b
Marif226 <marif226@yandex.ru>
2023-10-23 00:19:03 +0600

    Added migrations

---
+++ migrations/000001_create_people_table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE people (
+    id          int     PRIMARY KEY,
+    name        varchar(50),
+    surname     varchar(50),
+    patronymic  varchar(50),
+    age         int,
+    sex         varchar(5),
+    nationality varchar(50)
+);
