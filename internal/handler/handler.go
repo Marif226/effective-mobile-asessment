40885d40b62f0915a231c67981007e318e604cb6
Marif226 <marif226@yandex.ru>
2023-10-24 00:09:42 +0600

    Added routes initialization

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,6 +1,9 @@
 package handler
 
-import "github.com/Marif226/effective-mobile-assessment/internal/service"
+import (
+	"github.com/Marif226/effective-mobile-assessment/internal/service"
+	"github.com/go-chi/chi/v5"
+)
 
 type Handler struct {
 	services *service.Service
@@ -10,4 +13,14 @@ func New(services *service.Service) *Handler {
 	return &Handler{
 		services: services,
 	}
+}
+
+func (h *Handler) InitRoutes() *chi.Mux {
+	router := chi.NewRouter()
+
+	router.Route("/people", func(r chi.Router) {
+		r.Post("/", h.createPerson)
+	})
+
+	return router
 }
