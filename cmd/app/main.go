00cd80cf9e527824327316c9009dfbf93ad2d681
Marif226 <marif226@yandex.ru>
2023-10-24 13:48:50 +0600

    Added main app

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Marif226/effective-mobile-assessment/internal/handler"
+	"github.com/Marif226/effective-mobile-assessment/internal/repository"
+	"github.com/Marif226/effective-mobile-assessment/internal/service"
+)
+
 func main() {
-	
+	repository := repository.New(nil)
+	services := service.New(repository)
+	handlers := handler.New(services)
+
+	httpServer := http.Server{
+		Addr: ":8080",
+		Handler: handlers.InitRoutes(),
+		MaxHeaderBytes: 1 << 20, // 1 MB
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+	}
+
+	err := httpServer.ListenAndServe()
+	if err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
 }
