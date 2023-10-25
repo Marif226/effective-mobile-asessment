3ad7017f8efd32c4f05b29593e52c6f914e2e2f6
Marif226 <marif226@yandex.ru>
2023-10-26 02:12:23 +0600

    Added graceful shutdown for server

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"context"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
-	"github.com/joho/godotenv"
+
 	"github.com/Marif226/effective-mobile-assessment/internal/handler"
 	"github.com/Marif226/effective-mobile-assessment/internal/repository"
 	"github.com/Marif226/effective-mobile-assessment/internal/service"
+	"github.com/joho/godotenv"
 	"gopkg.in/yaml.v2"
+)
+
+const (
+	port = ":8080"
 )
 
 func main() {
@@ -44,17 +52,48 @@ func main() {
 	handlers := handler.New(services)
 
 	httpServer := http.Server{
-		Addr: ":8080",
+		Addr: port,
 		Handler: handlers.InitRoutes(),
 		MaxHeaderBytes: 1 << 20, // 1 MB
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 	}
 
+	// Server run context
+	serverCtx, serverStopCtx := context.WithCancel(context.Background())
+
+	// Listen for syscall signals for process to interrupt/quit
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	go func() {
+		<-sig
+
+		// Shutdown signal with grace period of 30 seconds
+		shutdownCtx, _ := context.WithTimeout(serverCtx, 30*time.Second)
+
+		go func() {
+			<-shutdownCtx.Done()
+			if shutdownCtx.Err() == context.DeadlineExceeded {
+				log.Fatal("graceful shutdown timed out.. forcing exit.")
+			}
+		}()
+
+		// Trigger graceful shutdown
+		err := httpServer.Shutdown(shutdownCtx)
+		if err != nil {
+			log.Fatal(err)
+		}
+		serverStopCtx()
+	}()
+
+	// Run the server
 	err = httpServer.ListenAndServe()
-	if err != nil {
-		log.Fatalf("error occured while running http server: %s", err.Error())
+	if err != nil && err != http.ErrServerClosed {
+		log.Fatal(err)
 	}
+
+	// Wait for server context to be stopped
+	<-serverCtx.Done()
 }
 
 // initialize config file, return error if failed
